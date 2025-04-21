1eef42206ea8a9daab3e736566a7b2cc4e8856ef
eeswepe <singgihwahyupermana@gmail.com>
2025-04-21 17:46:53 +0700

    finishing all task

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        hello("Singgih");
+    }
+
+    public static void hello(String name){
+        System.out.println("Hello " + name);
+    }
+    
+}
